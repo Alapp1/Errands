91b2da8c2380eba857c001d203ac1137dd69d723
Alapp1 <lappaiden@gmail.com>
2024-09-18 13:39:16 -0400

    initial commit

---
+++ src/main/java/com/errands/Errands_App/ErrandsAppApplication.java
@@ -0,0 +1,13 @@
+package com.errands.Errands_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrandsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrandsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/errands/Errands_App/ErrandsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errands.Errands_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrandsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
