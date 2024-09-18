1a825b0579a197f99c94485c555ec58fc56c95cf
Aiden Lapp <adlapp@ncsu.edu>
2024-09-18 15:50:19 -0400

    removed all sql references for initial construction

--- src/main/java/com/errands/Errands_App/ErrandsAppApplication.java
+++ src/main/java/com/errands/Errands_App/ErrandsAppApplication.java
@@ -2,12 +2,20 @@ package com.errands.Errands_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ErrandsAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ErrandsAppApplication.class, args);
 	}
+}
 
+@RestController
+class HelloWorldController {
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello, World!";
+	}
 }
